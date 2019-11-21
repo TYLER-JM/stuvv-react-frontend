ad0b2728a7c63a4edd9cc14421334a1bffcb6f72
Jordan Owens <jordanowens@JOadelic.local>
2019-11-21 15:48:01 -0500

    created first stage of sideBar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import TemporaryDrawer from "./SideBar/index"
 
 export default function Home() {
   return (
-    <h1>Welcome to the home page</h1>
+    <div>
+      <h1>Welcome to the home page</h1>
+      < TemporaryDrawer />
+    </div>
   );
 }
