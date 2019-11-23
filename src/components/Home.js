84b886be54ee520927eee246909ad0cff6044075
tyler martin <t.mar92@gmail.com>
2019-11-23 17:55:55 -0500

    define variable 'listing' to hold backend data

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,37 +2,79 @@ import React from 'react'
 import Form from './UploadForm'
 import SideBar from "./SideBar/index"
 import CardList from "./CardList"
+import ListingHelper from '../helpers/ListingHelper';
 
 
 export default function Home() {
 
+  // get the value from the helper and assign it to cardData
+
+  const listings = ListingHelper();
+  console.log("listing defined after calling helper", listings)
   const cardData = [
     {
       "title": "Tyler's Shoe",
       "description": "This is a nice shoe",
       "availability": true,
+      "image_urls": [
+        {
+          "url": "https://lorempixel.com/300/300/technics"
+        },
+        {
+          "url": "https://lorempixel.com/300/300/technics"
+        },
+      ]
     },
     {
       "title": "Tyler's Shoe",
       "description": "This is a nice shoe",
       "availability": true,
+      "image_urls": [
+        {
+          "url": "https://lorempixel.com/300/300/technics"
+        },
+        {
+          "url": "https://lorempixel.com/300/300/technics"
+        },
+      ]
     },
     {
       "title": "Tyler's Shoe",
       "description": "This is a nice shoe",
       "availability": true,
+      "image_urls": [
+        {
+          "url": "https://lorempixel.com/300/300/technics"
+        },
+        {
+          "url": "https://lorempixel.com/300/300/technics"
+        },
+        {
+          "url": "https://lorempixel.com/300/300/technics"
+        },
+      ]
     },
     {
       "title": "Tyler's Shoe",
       "description": "This is a nice shoe",
       "availability": true,
+      "image_urls": [
+        {
+          "url": "https://lorempixel.com/300/300/technics"
+        },
+        {
+          "url": "https://lorempixel.com/300/300/technics"
+        },
+      ]
     },
   ]
+
+
   return (
     <div>
       {/* <h1>Welcome to the home page</h1> */}
       {/* < SideBar /> */}
-      < CardList cardsData={cardData} />
+      < CardList cardsData={listings} />
       < Form />
     </div>
   );
