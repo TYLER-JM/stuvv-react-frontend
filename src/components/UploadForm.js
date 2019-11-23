49b79cb6dd60de5cb3feeca368c45769fa0b9113
Tamires <tamires.lowande@gmail.com>
2019-11-22 20:19:19 -0500

    adds the possibility to upload multiple images

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -5,23 +5,31 @@ export default function Form() {
 
 
   const [text, setText] = useState("");
-  const [images, setImages] = useState('');
+  const [images, setImages] = useState([]);
 
   console.log('image', images);
   const sendRequest = () => {
     console.log('got to the sendRequest function', images)
     const data = new FormData();
-    data.append('pics', images);
+    // data.append('pics', images);
+
+    for (let img of images) {
+      data.append('pics[]', img, img.name)
+    }
+
     data.append('title', text);
     data.append('user_id', 4);
-    console.log('DATA', data)
     let state = {
       "title": text,
       "pics": data,
       "user_id": 4
     }
-
+    // setTimeout(() => { console.log("this is the data variable", data) }, 3000);
+    console.log("IMAGES:", images)
+    console.log('DATA', data)
     return axios.post(`http://localhost:3000/listings`, data).then(resp => console.log("got to the then")).catch(error => console.error())
+
+    // return fetch('http://localhost:3000/listings', { method: 'POST', body: data }).then(resp => console.log("got to the then")).catch(error => console.error())
   }
 
   return (
@@ -29,10 +37,11 @@ export default function Form() {
       <h1>THE FORM</h1>
       <input type="text" onChange={event => setText(event.target.value)} />
       <input type="file" onChange={event => {
-        console.log('event files', event.target.files[0]);
-        setImages(event.target.files[0])
+        console.log('event files', event.target.files);
+        setImages(event.target.files)
       }} multiple />
-      <button onClick={() => sendRequest()}>submit</button>
+      {/* <button onClick={(event) => setImages(event.target.previousSibling.files)}> Upload!!</button> */}
+      <button onClick={() => sendRequest()}> submit</button>
     </form >
   );
 }
