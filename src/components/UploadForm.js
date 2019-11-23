b607b8682e430a31527fca3164151054fb5a57fe
Tamires <tamires.lowande@gmail.com>
2019-11-22 20:25:16 -0500

    cleaning code comments

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -7,9 +7,8 @@ export default function Form() {
   const [text, setText] = useState("");
   const [images, setImages] = useState([]);
 
-  console.log('image', images);
   const sendRequest = () => {
-    console.log('got to the sendRequest function', images)
+
     const data = new FormData();
     // data.append('pics', images);
 
@@ -19,17 +18,8 @@ export default function Form() {
 
     data.append('title', text);
     data.append('user_id', 4);
-    let state = {
-      "title": text,
-      "pics": data,
-      "user_id": 4
-    }
-    // setTimeout(() => { console.log("this is the data variable", data) }, 3000);
-    console.log("IMAGES:", images)
-    console.log('DATA', data)
-    return axios.post(`http://localhost:3000/listings`, data).then(resp => console.log("got to the then")).catch(error => console.error())
 
-    // return fetch('http://localhost:3000/listings', { method: 'POST', body: data }).then(resp => console.log("got to the then")).catch(error => console.error())
+    return axios.post(`http://localhost:3000/listings`, data).then(resp => console.log("got to the then")).catch(error => console.error())
   }
 
   return (
@@ -37,10 +27,8 @@ export default function Form() {
       <h1>THE FORM</h1>
       <input type="text" onChange={event => setText(event.target.value)} />
       <input type="file" onChange={event => {
-        console.log('event files', event.target.files);
         setImages(event.target.files)
       }} multiple />
-      {/* <button onClick={(event) => setImages(event.target.previousSibling.files)}> Upload!!</button> */}
       <button onClick={() => sendRequest()}> submit</button>
     </form >
   );
