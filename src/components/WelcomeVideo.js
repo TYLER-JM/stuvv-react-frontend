9b06d761a89dea375e067ae36a388db98b503e17
Jordan Owens <jordanowens@JOadelic.local>
2019-11-25 12:25:26 -0500

    jumbotron video/search

--- src/components/WelcomeVideo.js
+++ src/components/WelcomeVideo.js
@@ -21,7 +21,7 @@ export default function WelcomeVideo() {
           <form className="form-inline my-2 my-lg-0">
             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
             <button className="btn btn-light my-2 my-sm-0" type="submit">Search</button>
-         </form>
+          </form>
         </div>
 
 
