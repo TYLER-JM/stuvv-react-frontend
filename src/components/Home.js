1ea84df364e2db31c28c0d2d04a8e3a344277d1d
Jordan Owens <jordanowens@JOadelic.local>
2019-11-25 08:39:53 -0500

    adds jumbotron

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,14 @@ import React from 'react'
 import Form from './UploadForm'
 import SideBar from "./SideBar/index"
 import CardList from "./CardList"
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
+import Handshake from './handshake.jpg'
+import Video from './video.mp4'
+
+
+
+
 
 
 export default function Home() {
@@ -28,10 +36,37 @@ export default function Home() {
       "availability": true,
     },
   ]
+
+  var sectionStyle = {
+    width: "100%",
+    height: "400px",
+    backgroundImage: `url(${Handshake})`
+  };
+
   return (
     <div>
       {/* <h1>Welcome to the home page</h1> */}
-      {/* < SideBar /> */}
+      {/* <Jumbotron> */}
+      <Jumbotron  style={{  
+        backgroundImage: `url(${Handshake})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        opacity: .7,
+        color: "white",
+      }}> 
+        <video autoPlay loop muted style={{width: "100%",
+          height: "90%"}}>
+          <source src={Video} type='video/mp4' />
+        </video>
+       <h1>Hello, world!</h1>
+       <p>
+        Stuvv allows you to rent your things while you're not using them!
+       </p>
+       <p>
+         <Button variant="primary">Create A Listing</Button>
+       </p>
+      </Jumbotron> 
       < CardList cardsData={cardData} />
       < Form />
     </div>
