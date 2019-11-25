f80581b24734c4b81218fa50c5a8fbf6f904600b
Jordan Owens <jordanowens@JOadelic.local>
2019-11-25 16:25:23 -0500

    organizes navBar

--- src/components/WelcomeVideo.js
+++ src/components/WelcomeVideo.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Video from './video.mp4'
 // import Sample from './sample.MOV'
 import Sample from './samplesample.mp4'
+import axios from "axios";
 
 
 
 
 
 export default function WelcomeVideo() {
+
+  const [search, setSearch] = useState("");
+
+  const sendRequest = () => {
+    return axios.post(`http://localhost:3000/search`, { search }).then(resp => console.log("got to the then")).catch(error => console.error())
+  }
   return (
     <div>
       <header className="v-header container">
@@ -18,9 +25,16 @@ export default function WelcomeVideo() {
         <div className="header-content">
           <h1>Hello World</h1>
           <p>Stuvv allows you to rent your personal belonings</p>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-            <button className="btn btn-light my-2 my-sm-0" type="submit">Search</button>
+          <form className="form-inline my-2 my-lg-0" onSubmit={event => event.preventDefault()}>
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              onChange={e => setSearch(e.target.value)}
+            >
+            </input>
+            <button className="btn btn-light my-2 my-sm-0" type="submit" onClick={() => sendRequest()}>Search</button>
           </form>
         </div>
 
