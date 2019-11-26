082f154bebd90d9f34844e24c76f89043ca81345
tyler martin <t.mar92@gmail.com>
2019-11-26 17:18:29 -0500

    adds useEffect to make call to the backend

--- src/components/SideBar/MyStuvv.js
+++ src/components/SideBar/MyStuvv.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CardList from "../CardList.js"
 import './MyStuvv.scss'
 // import { getThemeProps } from '@material-ui/styles';
 import ListingHelper from '../../helpers/ListingHelper.js';
+import Axios from 'axios';
 
 
-export default function MyStuvv() {
-  const listings = ListingHelper()
+export default function MyStuvv(props) {
+  // const listings = ListingHelper()
+  // const listings = []
+
+  const [list, setList] = useState([])
+
+  useEffect(() => {
+    Axios.get(`http://localhost:3000/userslistings/${props.user.id}`, {withCredentials: true })
+      .then(resp => {
+        console.log("the users listings", resp.data);
+        setList(resp.data);
+        console.log("logged in as: ", props.user.id)
+      })
+      .catch(error => console.log(error))
+  },[])
 
   // const cardData = [
   //   {
@@ -31,5 +45,5 @@ export default function MyStuvv() {
   // const cards = props.cards.map((card) => {
 
   // return <ul className="my-stuvv-container">{cards}</ul>
-  return <CardList cardsData={listings} />
+  return <CardList cardsData={list} />
 }
