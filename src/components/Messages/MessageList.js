7580b1b488bfa4792eb7ff31aac131e9f8b3c7e3
Tamires <tamires.lowande@gmail.com>
2019-11-28 18:01:56 -0500

    starts adding live updates in messages

--- src/components/Messages/MessageList.js
+++ src/components/Messages/MessageList.js
@@ -8,11 +8,14 @@ export default function MessageList(props) {
   // console.log("the conversation inside the ML TYPE", JSON.parse(props.conversationObject.conversation))
 
   const [message, setMessage] = useState([]);
+  const [refresh, setRefresh] = useState([]);
 
   const sendMessage = function () {
     // event.preventDefault();
     // useEffect(() => {
     //send the single state as the data
+    setRefresh(message[message.length - 1])
+
     axios.put(`http://localhost:3000/messages/${props.conversationObject.id}`, { message: JSON.stringify(message) }, { withCredential: true })
       .then(resp => {
         console.log("got a response after updating the conversation");
@@ -24,8 +27,9 @@ export default function MessageList(props) {
 
   }
   const parsedConvo = JSON.parse(props.conversationObject.conversation)
-
+  // setMessage(parsedConvo)
   const bubbles = parsedConvo.map((msg, i) => {
+    // const bubbles = message.map((msg, i) => {
     return (
       <MessageListItem key={i} messageObject={msg} user={props.user} />
     )
@@ -35,7 +39,12 @@ export default function MessageList(props) {
     <li className={classNames({ "hidden": props.sentBy.first_name !== props.convo })}>
       {/* <p>{props.conversationObject.id}</p> */}
       {bubbles}
-      {/* <input type="text" onChange={e => setSingle([single[single], content: e.target.value ])} /> */}
+      {/* in case we want live messages
+      <div className="message-list-item">
+        <span>{refresh.sender} says: </span>
+        <span>{refresh.content}</span>
+      </div> */}
+
       <input type="text" onChange={e => setMessage([...parsedConvo,
       {
         sender: props.user.first_name, //current logged in user
