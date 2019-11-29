17114aa6a7b5a8cf7d6c509af801e6add8d28ae8
Tamires <tamires.lowande@gmail.com>
2019-11-29 15:43:02 -0500

    all routes depend on login but home '/'

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import MessageList from './MessageList';
 import MessagesSideBar from './MessagesSideBar';
 import './Messages.scss';
+import Register from '../Login/RegisterModal';
 
 
 export default function Messages(props) {
@@ -61,13 +62,13 @@ export default function Messages(props) {
 
   const conversations = messages.map((conversation, i) => {
     return (
-      <MessageList 
-        key={i} 
-        conversationObject={conversation} 
+      <MessageList
+        key={i}
+        conversationObject={conversation}
         // listingObject={conversation.listing}
-        sentBy={conversation.from_user} 
-        convo={convo} 
-        user={props.user} 
+        sentBy={conversation.from_user}
+        convo={convo}
+        user={props.user}
         // uniqueid={`${conversation.listing.title}${conversation.to_user.first_name}${conversation.from_user.first_name}`}
         uniqueid={`convo${i}`}
       />
@@ -76,45 +77,52 @@ export default function Messages(props) {
 
   const names = messages.map((conversation, i) => {
     return (
-      <MessagesSideBar 
-        key={i} 
-        sentBy={conversation.from_user} 
-        setConvo={setConvo} convo={convo} 
-        listingObject={conversation.listing} 
+      <MessagesSideBar
+        key={i}
+        sentBy={conversation.from_user}
+        setConvo={setConvo} convo={convo}
+        listingObject={conversation.listing}
         user={props.user}
         toUser={conversation.to_user}
         uniqueid={`convo${i}`}
       />
     )
   })
-  return (
-    <div>
-      <div className="messagesBanner">
-        Messages
+
+  if (props.user.id) {
+    return (
+      <div>
+        <div className="messagesBanner">
+          Messages
+        </div>
+        <div className="messages-container">
+          <div className="tab">
+            <span onClick={() => {
+              fetchMessages("inbound")
+            }}>inbound</span>
+            <span onClick={() => {
+              fetchMessages("outbound")
+            }}>outbound</span>
+          </div>
+          <div className="side-bar-body">
+            {names}
+
+          </div>
+          <div className="message-list-body">
+            <ul className="message-list">
+              {conversations}
+            </ul>
+          </div>
+        </div>
+
+
+
+        {/* <button onClick={() => setChange(2)}>GET MESSAGES</button> */}
       </div>
-      <div className="messages-container">
-        <div className="tab">
-          <span onClick={() => {
-            fetchMessages("inbound")
-          }}>inbound</span>
-          <span onClick={() => {
-            fetchMessages("outbound")
-          }}>outbound</span>
-        </div>
-        <div className="side-bar-body">
-          {names}
+    );
 
-        </div>
-        <div className="message-list-body">
-          <ul className="message-list">
-            {conversations}
-          </ul>
-        </div>
-      </div>
+  } else {
+    return (<Register show="true" onHide={() => window.location.pathname = "/"} />)
 
-
-
-      {/* <button onClick={() => setChange(2)}>GET MESSAGES</button> */}
-    </div>
-  );
+  }
 }
--- src/components/MyRequests/MyRequests.js
+++ src/components/MyRequests/MyRequests.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './Myrequests.scss'
 import CardList from "../CardList.js"
-// import 'components/Sidebar/MyStuvv.scss'
+import Register from '../Login/RegisterModal';
 
 
 
 export default function MyRequests(props) {
+  if (props.user.id) {
+    return (
+      <div>
+        <div className="requests-banner">My Requests</div>
+        <CardList cardsData={props.request} user={props.user} />
 
-  return (
-    <div>
-      <div className="requests-banner">My Requests</div>
-      <CardList cardsData={props.request} user={props.user} />
+      </div>
+    )
+  } else {
+    return (<Register show="true" onHide={() => window.location.pathname = "/"} />)
 
-    </div>
-  )
+  }
 }
