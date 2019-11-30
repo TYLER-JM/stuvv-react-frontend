2de03ba8d0109a2c71205403ed2e7b5fb36ae8e9
Tamires <tamires.lowande@gmail.com>
2019-11-30 11:11:01 -0500

    accepts a request inside a message

--- src/components/Messages/MessageList.js
+++ src/components/Messages/MessageList.js
@@ -2,13 +2,20 @@ import React, { useState, useEffect } from 'react'
 import MessageListItem from './MessageListItem';
 import axios from 'axios';
 import classNames from 'classnames';
+import Button from 'react-bootstrap/Button'
 import './Messages.scss'
 
 
 export default function MessageList(props) {
+
   console.log("the unique identifier: ", props.uniqueid)
+
   const [conversation, setConversation] = useState(JSON.parse(props.conversationObject.conversation));
   const [message, setMessage] = useState("");
+  // const [accept, setAccept] = useState((<div className="buttons-message">
+  //   <Button variant="outline-success" onClick={handleAccept}>Accept</Button>
+  //   <Button variant="outline-secondary">Decline</Button>
+  // </div>));
 
   const reset = (e) => {
     setMessage("")
@@ -24,10 +31,20 @@ export default function MessageList(props) {
     reset(e)
   }
 
+  const handleAccept = function () {
+    return axios.patch(`http://localhost:3000/requests/${props.conversationObject.request.id}`, { request: { approved: true } }, { withCredentials: true })
+      .then(resp => {
+        console.log("GOT OUTBOUND MESSAGES: ", resp.data);
+        // setAccept(resp.data)
+      })
+      .catch(error => console.log(error))
+  }
+
+
   useEffect(() => {
     axios.put(`http://localhost:3000/messages/${props.conversationObject.id}`, { message: JSON.stringify(conversation) }, { withCredential: true })
-    .then(resp => {
-      console.log("got a response after updating the conversation");
+      .then(resp => {
+        console.log("got a response after updating the conversation");
       })
       .catch(err => {
         console.log("error is: ", err);
@@ -35,7 +52,7 @@ export default function MessageList(props) {
   }, [conversation])
 
 
-    const bubbles = conversation.map((msg, i) => {
+  const bubbles = conversation.map((msg, i) => {
     return (
       <MessageListItem key={i} messageObject={msg} user={props.user} />
     )
@@ -44,9 +61,14 @@ export default function MessageList(props) {
   return (
     <li className={classNames({ "hidden": props.uniqueid !== props.convo })}>
       {bubbles}
+      {!props.conversationObject.request.approved ? (<div className="buttons-message">
+        <Button variant="outline-success" onClick={handleAccept}>Accept</Button>
+        <Button variant="outline-secondary">Decline</Button>
+      </div>) : null}
+
       <div className="search-input">
         <input className="input-field" type="text" value={message} onChange={e => setMessage(e.target.value)} />
-        <button className="button" onClick={(e) => sendMessage(e)}>SEND!</button>
+        <button className="send" onClick={(e) => sendMessage(e)}>SEND!</button>
       </div>
     </li>
   );
